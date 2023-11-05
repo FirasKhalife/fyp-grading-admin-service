b20adf9160c708a5cbe5321bba86f32832a86c22
Gaelle327 <gaellesaid65@gmail.com>
2023-11-05 19:24:35 +0200

    adding hypstix

--- src/main/java/com/fypgrading/adminservice/AdminServiceApplication.java
+++ src/main/java/com/fypgrading/adminservice/AdminServiceApplication.java
@@ -2,8 +2,12 @@ package com.fypgrading.adminservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableCircuitBreaker
+@EnableEurekaClient
 public class AdminServiceApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/fypgrading/adminservice/service/RubricService.java
+++ src/main/java/com/fypgrading/adminservice/service/RubricService.java
@@ -4,6 +4,7 @@ import com.fypgrading.adminservice.service.dto.RubricDTO;
 import com.fypgrading.adminservice.entity.Rubric;
 import com.fypgrading.adminservice.service.mapper.RubricMapper;
 import com.fypgrading.adminservice.repository.RubricRepository;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.stereotype.Service;
 
@@ -20,16 +21,20 @@ public class RubricService {
         this.rubricMapper = rubricMapper;
     }
 
+    @HystrixCommand(fallbackMethod = "handleGetRubricsFallback")
     public List<RubricDTO> getRubrics() {
         List<Rubric> rubrics = rubricRepository.findAll();
         return rubricMapper.toDTOList(rubrics);
     }
 
+    @HystrixCommand(fallbackMethod = "handleCreateRubricFallback")
     public RubricDTO createRubric(RubricDTO rubricDTO) {
         Rubric rubric = rubricMapper.toEntity(rubricDTO);
         Rubric createdEntity = rubricRepository.save(rubric);
         return rubricMapper.toDTO(createdEntity);
     }
+
+    @HystrixCommand(fallbackMethod = "handleUpdateRubricFallback")
 
     public RubricDTO updateRubric(Integer id, RubricDTO rubricDTO) {
         getRubricById(id);
@@ -38,6 +43,8 @@ public class RubricService {
         Rubric updatedEntity = rubricRepository.save(rubric);
         return rubricMapper.toDTO(updatedEntity);
     }
+
+    @HystrixCommand(fallbackMethod = "handleDeleteRubricFallback")
 
     public RubricDTO deleteRubric(Integer id) {
         Rubric rubric = getRubricById(id);
@@ -48,5 +55,21 @@ public class RubricService {
     private Rubric getRubricById(Integer id) {
         return rubricRepository.findById(id).orElseThrow(() ->
                         new EntityNotFoundException("Rubric not found"));
+    }
+
+    public List<RubricDTO> handleGetRubricsFallback() {
+        return List.of();
+    }
+
+    public RubricDTO handleCreateRubricFallback(RubricDTO rubricDTO) {
+        return new RubricDTO();
+    }
+
+    public RubricDTO handleUpdateRubricFallback(Integer id, RubricDTO rubricDTO) {
+        return rubricDTO;
+    }
+
+    public RubricDTO handleDeleteRubricFallback(Integer id) {
+        return new RubricDTO();
     }
 }
