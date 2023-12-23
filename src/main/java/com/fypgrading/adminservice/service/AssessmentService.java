ccc44830ea031a560d83148e137951455ad9ac3b
Gaelle327 <gaellesaid65@gmail.com>
2023-12-23 22:37:21 +0200

    new pr

--- src/main/java/com/fypgrading/adminservice/service/AssessmentService.java
+++ src/main/java/com/fypgrading/adminservice/service/AssessmentService.java
@@ -10,7 +10,6 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class AssessmentService {
-
     private static final Logger LOGGER = LoggerFactory.getLogger(AssessmentService.class);
 
     private final AssessmentRepository assessmentRepository;
